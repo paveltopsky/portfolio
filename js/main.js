02e572e2520a3e76538ca9507fb8ff89d4bd41e8
paveltopsky <55711291+paveltopsky@users.noreply.github.com>
2020-05-07 23:30:50 +0300

    new font; edit parallax, main + media

---
+++ js/main.js
@@ -0,0 +1,55 @@
+let p1 = document.getElementById('p1');
+let p2 = document.getElementById('p2');
+let p3 = document.getElementById('p3');
+let p4 = document.getElementById('p4');
+let p5 = document.getElementById('p5');
+let p6 = document.getElementById('p6');
+let p7 = document.getElementById('p7');
+let p8 = document.getElementById('p8');
+let p9 = document.getElementById('p9');
+let p10 = document.getElementById('p10');
+let p11 = document.getElementById('p11');
+let p1Instance = new Parallax(p1, {
+  invertY: true,
+  invertX: false,
+});
+let p2Instance = new Parallax(p2, {
+  invertY: true,
+  invertX: false,
+});
+let p3Instance = new Parallax(p3, {
+  invertY: false,
+  invertX: true,
+});
+let p4Instance = new Parallax(p4, {
+  invertY: true,
+  invertX: false,
+});
+let p5Instance = new Parallax(p5, {
+  invertY: true,
+  invertX: false,
+});
+let p6Instance = new Parallax(p6, {
+  invertY: false,
+  invertX: true,
+});
+let p7Instance = new Parallax(p7, {
+  invertY: true,
+  invertX: false,
+});
+let p8Instance = new Parallax(p8, {
+  invertY: true,
+  invertX: false,
+});
+let p9Instance = new Parallax(p9, {
+  invertY: false,
+  invertX: true,
+});
+let p10Instance = new Parallax(p10, {
+  invertY: false,
+  invertX: true,
+});
+let p11Instance = new Parallax(p11, {
+  invertY: false,
+  invertX: true,
+});
